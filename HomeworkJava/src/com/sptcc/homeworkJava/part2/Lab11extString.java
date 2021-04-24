0c23e4fd6233a6194867e1f5b53c073a8a119f9e
Kittanut T <tkittanut@gmail.com>
2021-04-24 13:47:04 +0700

    finished p2 lab11 ext

---
+++ HomeworkJava/src/com/sptcc/homeworkJava/part2/Lab11extString.java
@@ -0,0 +1,10 @@
+package com.sptcc.homeworkJava.part2;
+
+public class Lab11extString {
+	public static void main(String [] args) {
+		char[] myChar = {'C','h','a','r',' ','t','o',' ','S','t','r','i','n','g'};
+		String myWord = new String(myChar);
+		System.out.println(myWord);
+	}
+
+}
